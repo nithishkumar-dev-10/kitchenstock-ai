1f0db48a12d4768e4fbddbf4d829df2f526e0c87
nithish <nithishkumar-dev10@gmail.com>
2026-03-28 16:03:38 +0530

    structred json format,fixed engine

---
+++ kitchen/services/a.py
@@ -0,0 +1,7 @@
+inventory = {
+    "rice": {
+        "quantity": 5,
+        "unit": "kg"
+    }
+}
+inv=inventory.items()
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from kitchen.services.dish_checker import check_ingredients
+from kitchen.services.dish_engine import cook_dish
 
+# Step 1: Check
+check_ingredients("fried_rice", 2)
+
+# Step 2: Cook
+cook_dish("fried_rice", 1)
