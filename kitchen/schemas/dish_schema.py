cae8db5821ed1a105a08dc5de951f884e14eb7ad
Nithish Kumar Saravanan <nithishkumar.dev10@gmail.com>
2026-04-07 14:50:46 +0530

    added the field for data validation

--- kitchen/schemas/dish_schema.py
+++ kitchen/schemas/dish_schema.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class DishInput(BaseModel):
-    dish_name: str = Field(..., min_length=1, example="biryani")
+    dish_name: str = Field(..., min_length=1, example="fried_rice")
     servings: int = Field(..., ge=1, example=2)
 
 class DishCheckInput(BaseModel):
-    dish_name: str = Field(..., min_length=1, example="biryani")
+    dish_name: str = Field(..., min_length=1, example="fried_rice")
     servings: int = Field(..., ge=1, example=2)
 
 class InventorySnapshot(BaseModel):
